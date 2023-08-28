baa2a52fc6198a0d0eeaa494cc8927f22a233e5c
ArielGimenez03 <140121967+ArielGimenez03@users.noreply.github.com>
2023-08-27 22:05:14 -0300

    Add files via upload

---
+++ html/script/script.js
@@ -0,0 +1,11 @@
+var nombre = "Ariel";
+var apellido = "Gimenez";
+
+console.log(nombre + " " + apellido);
+
+var numUno = 3; 
+var numDos = 4;
+var suma = (numUno + numDos);
+console.log("La suma es: " + suma);
+var suma1 = Number(suma + (3));
+console.log("El resultado de la segunda suma es: " + suma1);
